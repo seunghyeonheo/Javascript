b2b8de1b296231f895a4c8915ee48d19004564ed
seunghyeonheo <seunghyun9615@gmail.com>
2022-06-01 14:05:38 +0900

    .

---
+++ javascript/2.naming.js
@@ -0,0 +1,35 @@
+// 주석을 달면 됨
+/** 
+ * 주석이 됨
+ * 다음줄로 자동으로 넘어감
+ */
+
+/**
+ * 변수 규칙!
+ * 라틴문자(0~9, a~z, A~Z), _
+ * 대소문자를 구분함
+ * 추천: camelCase (likeThis)
+ * 여러 개의 변수를 1,2,3 숫자로 구분x, -> 최대한 의미있게 구체적인 이름으로 작성!
+ */
+
+let apple;
+let red;
+let this;
+
+//나쁜 예제
+let number = 20;
+
+//좋은 예제
+let myAge = 24;
+
+//나쁜 예제
+let member1;
+let member2;
+
+//좋은 예제
+let firstOrder;
+let lastOrder;
+
+//꿑팁! (검색하기 쉬움)
+let orderFirst;
+let orderLast;
---
+++ javascript/4.string.js
@@ -0,0 +1,18 @@
+//문자열 타입
+
+let string = "안녕하세요"
+
+let myName = "seunghyeon";
+
+myName = "Hello\nthis is seunghyeon\t\t나는\\코딩하는 중이야";
+
+console.log(myName);
+
+let id = "emily";
+let greeting = "안녕!" + id;
+console.log(greeting);
+
+greeting = `안녕! ${id}
+오늘 하루도 잘 보내!`;
+
+console.log(greeting);
