e856620aab9fbdfc30ef2d7f9102cd30cce02b00
seunghyeonheo <seunghyun9615@gmail.com>
2022-06-05 23:58:08 +0900

    .

--- javascript/basic.js
+++ javascript/basic.js
@@ -1,8 +1,16 @@
-function add(num1, num2){
+//사용예제1
+function sum(num1, num2){
     return num1 + num2;
 }
 
 //호출한 사람이 없으면 함수는 실행되지 않음
 
-const result = add(1,2);
+const result = sum(1,2);
 console.log(result);
+
+// 사용예제2
+
+let lastName = "이";
+let firstName = "민형";
+let fullName = `${lastName} ${firstName}`;
+console.log(fullName);
